c630a0458fa790230fc25e9de5a505ccc5d5ddbd
Михаил Подольский <podolskiy.mikhail@inessoft.kz>
2016-07-14 08:37:36 +0600

    [J8] Ключевое слово ::

--- src/main/java/kz/mix/java8/ConverterImpl.java
+++ src/main/java/kz/mix/java8/ConverterImpl.java
@@ -10,9 +10,14 @@ public class ConverterImpl {
             }
         };
 
+        // Упрощенный вариант с лябдой
         Converter<String, Integer> converter8 = (from) -> Integer.valueOf(from);
+
+        // Вариант с ссылкой на метод при помощи ::
+        Converter<String, Integer> converter8ref = Integer::valueOf;
 
         System.out.println(converter8.convert("123"));
         System.out.println(converter7.convert("12"));
+        System.out.println(converter8ref.convert("1"));
     }
 }
